d62f409adb727fe700e0dffddde5baab8f962240
Ofek <ofekcohen977@gmail.com>
2025-05-09 22:11:21 +0300

    undo test

--- src/App.jsx
+++ src/App.jsx
@@ -91,7 +91,7 @@ function App() {
       </div>
 
       <div className="copyContainer">
-      <button class="copyButton" onClick={copyToClipboard}>Copy copy</button>
+      <button class="copyButton" onClick={copyToClipboard}>Copy Text</button>
       </div>
 
     </div>

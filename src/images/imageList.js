a549f1b3d860e85d05ebd0c3edfdca28665492cb
Ofek <ofekcohen977@gmail.com>
2025-05-09 12:08:53 +0300

    added images

---
+++ src/images/imageList.js
@@ -0,0 +1,15 @@
+import image1 from './1KG.jpg';
+import image2 from './25KG.jpg';
+import image3 from './BB.jpg';
+import image4 from './kosher.jpg';
+import image5 from './BB-kosher.jpg';
+
+const imagesList = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+];
+
+export default imagesList;
